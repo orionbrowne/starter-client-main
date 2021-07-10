5da78f0af148c3ebcad5968f2f6539de81eb07b8
orionbrowne <orion.browne@gmail.com>
2021-07-10 15:58:48 -0400

    updated student view

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -3,7 +3,18 @@ const StudentView = (props) => {
   return (
     <div>
       <h1>{student.firstname + " " + student.lastname}</h1>
-      <h3>{student.campus.name}</h3>
+      <div>
+        <img src ={student.imageUrl} width="200" height="200"></img>
+
+      </div>
+      <div>
+      {student.email}
+
+       <h2>GPA = {student.gpa}</h2>
+      </div>
+      <h3>
+      {student.campus.name}
+      </h3>
     </div>
   );
 
