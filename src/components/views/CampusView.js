1b6620ad5d514be79dc9b27bdf76785a8cc853ae
orionbrowne <orion.browne@gmail.com>
2021-07-10 15:51:02 -0400

    updated single campus view

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -3,9 +3,11 @@
 const CampusView = (props) => {
   const {campus} = props;
   return (
-    <div>      
+    <div>
       <h1>{campus.name}</h1>
       <p>{campus.description}</p>
+      <p>{campus.address}</p>
+      <img src ={campus.imageUrl} width="200" height="200"></img>
       <ul>
       {campus.students.map( student => {
         let name = student.firstname + " " + student.lastname;
